0624093096073a3577ba5a9f692ea70a29461b7a
Innovator <shahriar.firoz@gmail.com>
2018-12-01 05:49:32 -0600

    Establishing JAWSDB connection parameters

--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,19 @@ var mysql = require("mysql");
 require('dotenv').config();
 var keys = require("../keys.js");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: keys.databasecredentials.user,
-    password: keys.databasecredentials.password,
-    database: "burgers_db"
-})
+var connection;
+
+if (process.env.JAWSDB_URL){
+    connection. mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: keys.databasecredentials.user,
+        password: keys.databasecredentials.password,
+        database: "burgers_db"
+    });
+};
 
 connection.connect(function (err) {
     if (err) {
